482b818f3809ba7297d5fcc37724c8843ac5bcf9
devops <devops@mycompany.com>
2018-06-04 10:50:41 +0530

    wrote my first shell script in myscript.sh

---
+++ scripts/myscript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##############################################
+#Purpos : To learn echo command in this first script
+#Version: 1.0
+#Owner	: Aditya P <adityapatel@mycompany.com>
+#Input	: None
+#Output	: some output from echo command
+###############################################
+
+echo "-----welcome to bash shell scripting----------"
+
+sleep 1    #wait for 1 seconds
+echo "5"
+
+sleep 1    #wait for 1 seconds
+echo "4"
+
+sleep 1    #wait for 1 seconds
+echo "3"
+
+sleep 1    #wait for 1 seconds
+echo "2"
+
+sleep 1    #wait for 1 seconds
+echo "1"
+
+echo "------script end----------"
