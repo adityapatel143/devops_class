84b5772ed6348c759d9d6a468750fd780eda4708
devops <devops@mycompany.com>
2018-06-04 10:39:33 +0530

    added the templet.sh file

---
+++ scripts/templeat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##############################################
+#Purpos : To learn shell script
+#Version: 1.0
+#Owner	: Aditya P <adityapatel@mycompany.com>
+#Input	: None
+#Output	: None
+###############################################
+
+
