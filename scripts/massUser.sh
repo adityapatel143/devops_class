00a8091a37459aea8f8599fc6b2c07ca06bfaa5f
devops <devops@mycompany.com>
2018-06-09 13:12:32 +0530

    created multiple users using script

--- scripts/massUser.sh
+++ scripts/massUser.sh
@@ -10,17 +10,37 @@
 
 USERLIST=$1
 
+#checking if user is root or not
+if [ $(id -u) != 0 ]; then 		# $(id -u) will provide the user id no ex. 1002, for root it is 0
+	echo "Please run the script as a ROOT user"
+	exit
+fi
+
+#checkiog if file is provided or not
 if [ ! "$USERLIST" ]; then
 	echo "Please provide the file having users name"
 	exit
 fi
 
+#checking if provided file type is correct or not
 if [ ! -f "$USERLIST" ]; then
 	echo "Provided file type is not correct"
 	exit
 fi
 
+# reading the names from the file using loop
 for OURUSER in `cat $USERLIST`
 do
-	echo " name of user is : $OURUSER"
+	echo " creating user : $OURUSER"
+	echo -e "\n----------------------------------"
+	#calling the script which can add the user(already created)
+	./addUser.sh $OURUSER
+	echo -e "\n----------------------------------"
 done
+
+# checking if all the users creaed successfully
+if [ "$?" -eq 0 ]; then
+	echo -e "\n -- ** Users created successfully ** --"
+else
+	echo -e "\n !!! Failed creating All/some users !!!"
+fi
